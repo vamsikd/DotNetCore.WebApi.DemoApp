0488d0eeb5f71f7127c16dcee608c910b7a8d19d
Vamsi Krishna D <vamsi.doddapaneni@outlook.com>
2023-08-11 16:24:00 +0530

    Changes the Get method mapping

--- DemoApp.API/Controllers/WeatherForecastController.cs
+++ DemoApp.API/Controllers/WeatherForecastController.cs
@@ -18,7 +18,7 @@ namespace DemoApp.API.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
+        [HttpGet(Name = "GetWeatherForecastTest")]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
